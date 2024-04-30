7a1d05bb397b9ecfa5bd589033980563f81bff86
Basilio <daniel.isaes@mail.ru>
2024-04-30 12:03:48 +0300

    Реализация конструктора

--- src/Employee.java
+++ src/Employee.java
@@ -4,6 +4,19 @@ public class Employee {
     private String lastNameEmployee;
     private int departmentEmployee;
     private float salaryEmployee;
+    private int idEmployee;
+    private static int idCounter = 0; // Счечик для выдачи id сотруднику
+
+    public Employee(String surNameEmployee, String nameEmployee, String lastNameEmployee, int departmentEmployee, float salaryEmployee) {
+        this.surNameEmployee = surNameEmployee;
+        this.nameEmployee = nameEmployee;
+        this.lastNameEmployee = lastNameEmployee;
+        this.departmentEmployee = departmentEmployee;
+        this.salaryEmployee = salaryEmployee;
+        this.idEmployee = idCounter;
+        idCounter++;
+    }
+
 
 
 }
