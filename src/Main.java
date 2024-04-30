1adab22e3a503a819cd667385061874b0fe92755
Basilio <daniel.isaes@mail.ru>
2024-04-30 14:26:58 +0300

    Заполнение списка сотрудников

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
+    // Заполнение списка сотрудников
+    public static void fillingListEmployees(Employee[] Employee, String[] surnameEmployee, String[] nameEmployee, String[] lastnameEmployee, int[] departmentEmployee, float[] salaryEmployee) {
+        for (int i = 0; i < Employee.length; i++) {
+            Employee[i] = new Employee(surnameEmployee[i], nameEmployee[i], lastnameEmployee[i], departmentEmployee[i], salaryEmployee[i]);
+        }
+    }
+
+
     public static void main(String[] args) {
         String[] nameEmployee = {"Ульяна", "Михаил", "Платон", "Надежда", "Эмилия", "Георгий", "Дмитрий", "Александра", "Валерия", "Максим"};
         String[] surnameEmployee = {"Моисеева", "Белоусов", "Крылов", "Мартынова", "Наумова", "Наумов", "Сидоров", "Золотова", "Макарова", "Фомин"};
@@ -6,6 +14,8 @@ public class Main {
         int[] departmentEmployee = {4, 4, 1, 1, 1, 5, 2, 1, 3, 3};
         float[] salaryEmployee = {81829f, 143553f, 81263f, 41372f, 83888f, 37498f, 146755f, 26775f, 115737f, 132367f};
         Employee[] Employee = new Employee[10];
+
+        fillingListEmployees(Employee, surnameEmployee, nameEmployee, lastnameEmployee, departmentEmployee, salaryEmployee);
 
 
     }
