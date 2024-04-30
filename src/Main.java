f6675cf1403e589a6f3d830b9ef542585bb5f26b
Basilio <daniel.isaes@mail.ru>
2024-04-30 16:35:21 +0300

    Изменение имени массива Employee на формат employeeList в методах, добавлен метод вывода ФИО

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,62 @@
 public class Main {
     // Заполнение списка сотрудников
-    public static void fillingListEmployees(Employee[] Employee, String[] surnameEmployee, String[] nameEmployee, String[] lastnameEmployee, int[] departmentEmployee, float[] salaryEmployee) {
-        for (int i = 0; i < Employee.length; i++) {
-            Employee[i] = new Employee(surnameEmployee[i], nameEmployee[i], lastnameEmployee[i], departmentEmployee[i], salaryEmployee[i]);
+    public static void fillingListEmployees(Employee[] employeeList, String[] surnameEmployee, String[] nameEmployee, String[] lastnameEmployee, int[] departmentEmployee, float[] salaryEmployee) {
+        for (int i = 0; i < employeeList.length; i++) {
+            employeeList[i] = new Employee(surnameEmployee[i], nameEmployee[i], lastnameEmployee[i], departmentEmployee[i], salaryEmployee[i]);
         }
     }
 
     //Вывод списка сотрудников
-    public static void displayingListEmployees(Employee[] Employee) {
-        for (int i = 0; i < Employee.length; i++) {
-            System.out.println(Employee[i].toString());
+    public static void displayingListEmployees(Employee[] employeeList) {
+        for (int i = 0; i < employeeList.length; i++) {
+            System.out.println(employeeList[i].toString());
         }
     }
 
     //  Сумму затрат на ЗП в месяц
-    public static float summMonthlyExpenses(Employee[] Employee) {
+    public static float summMonthlyExpenses(Employee[] employeeList) {
         float amountCosts = 0;
-        for (int i = 0; i < Employee.length; i++) {
-            amountCosts += Employee[i].getSalaryEmployee();
+        for (int i = 0; i < employeeList.length; i++) {
+            amountCosts += employeeList[i].getSalaryEmployee();
         }
         return amountCosts;
     }
 
     // Сотрудник с минимальной ЗП
-    public static String minimumWageEmployee(Employee[] Employee) {
+    public static String minimumWageEmployee(Employee[] employeeList) {
         String minimumWageEmployee_Name = null;
         float minimumWageEmployee_Salary = 1_000_000f;
-        for (int i = 0; i < Employee.length; i++) {
-            if (minimumWageEmployee_Salary > Employee[i].getSalaryEmployee()) {
-                minimumWageEmployee_Salary = Employee[i].getSalaryEmployee();
-                minimumWageEmployee_Name = Employee[i].toString();
+        for (int i = 0; i < employeeList.length; i++) {
+            if (minimumWageEmployee_Salary > employeeList[i].getSalaryEmployee()) {
+                minimumWageEmployee_Salary = employeeList[i].getSalaryEmployee();
+                minimumWageEmployee_Name = employeeList[i].toString();
             }
         }
         return minimumWageEmployee_Name;
     }
 
     // Сотрудник с максимальной ЗП
-    public static String maximumWageEmployee(Employee[] Employee) {
+    public static String maximumWageEmployee(Employee[] employeeList) {
         String maximumWageEmployee_Name = null;
         float maximumWageEmployee_Salary = 0f;
-        for (int i = 0; i < Employee.length; i++) {
-            if (maximumWageEmployee_Salary < Employee[i].getSalaryEmployee()) {
-                maximumWageEmployee_Salary = Employee[i].getSalaryEmployee();
-                maximumWageEmployee_Name = Employee[i].toString();
+        for (int i = 0; i < employeeList.length; i++) {
+            if (maximumWageEmployee_Salary < employeeList[i].getSalaryEmployee()) {
+                maximumWageEmployee_Salary = employeeList[i].getSalaryEmployee();
+                maximumWageEmployee_Name = employeeList[i].toString();
             }
         }
         return maximumWageEmployee_Name;
     }
 
-    public static float averageSalaryEmployees(Employee[] Employee) {
+    public static float averageSalaryEmployees(Employee[] employeeList) {
         float averageSalaryEmployees;
-        averageSalaryEmployees = summMonthlyExpenses(Employee) / Employee.length;
+        averageSalaryEmployees = summMonthlyExpenses(employeeList) / employeeList.length;
         return averageSalaryEmployees;
+    }
+    private static void outputFullName(Employee[] employeeList){
+        for (int i = 0; i < employeeList.length; i++) {
+            System.out.println(employeeList[i].getLastnameEmployee() + " " + employeeList[i].getNameEmployee() + " " + employeeList[i].getLastnameEmployee());
+        }
     }
 
     public static void main(String[] args) {
@@ -68,10 +73,8 @@ public class Main {
         System.out.println("Сотрудник с минимальной ЗП: " + minimumWageEmployee(employeeList));
         System.out.println("Сотрудник с максимальной ЗП: " + maximumWageEmployee(employeeList));
         System.out.println("Среднее значение ЗП: " + averageSalaryEmployees(employeeList));
+        outputFullName(employeeList);
 
-        for (int i = 0; i < employeeList.length; i++) {
-            System.out.println(employeeList[i].getLastnameEmployee() + " " + employeeList[i].getNameEmployee() + " " + employeeList[i].getLastnameEmployee());
-        }
 
     }
 }
