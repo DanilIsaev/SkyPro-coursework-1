5dd246e4be0ebe1f6d102006896aba9cd9903129
Basilio <daniel.isaes@mail.ru>
2024-04-30 11:52:25 +0300

    Создали класс employee

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    private String nameEmployee;
+    private String surNameEmployee;
+    private String lastNameEmployee;
+    private int departmentEmployee;
+    private float salaryEmployee;
+
+
+}
